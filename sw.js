8456a7e58e3076e733a1584fa1192fd6c54df689
RohitMohanty <rohitmohanty19@gmail.com>
2018-09-05 19:22:47 +0530

    added more societies v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME= 'static-v6';
-var CACHE_DYNAMIC_NAME= 'dynamic-v6';
+var CACHE_STATIC_NAME= 'static-v7';
+var CACHE_DYNAMIC_NAME= 'dynamic-v7';
   self.addEventListener('install',function(event){
   console.log('[Service Worker] Installing Service Worker ...',event);
 
