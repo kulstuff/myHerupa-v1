c81fbba4d5e4fb691f88229a76c3b5f3b51e0456
aadi <aadiyaara@gmail.com>
2019-01-12 12:28:10 +0530

    JSON Restructure

---
+++ src/Components/DropDown.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function DropDown({props}) {
+    var timesRender = props.number
+    switch(props.dropAction) {
+        case "Year":
+            const yearList = ["First" , "Second"];
+    }
+    return (
+        <div>
+            {
+                
+            }
+        </div>
+    )
+}
