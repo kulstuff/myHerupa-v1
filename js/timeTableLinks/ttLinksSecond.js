d43df5a85b994073b75371cac74881736715cc34
aadi <aadiyaara@gmail.com>
2019-07-31 12:40:30 +0530

    react premature

---
+++ js/timeTableLinks/ttLinksSecond.js
@@ -0,0 +1,105 @@
+const COE2 = [
+  "https://calendar.google.com/calendar?cid=NXA2Z3RsMTVwOWhuZ3I2NzNhMmtvNXFzb2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=YTZucnZ0NmwxZW9qZWFhNW03Mm5zbzhhOGNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dmJuamx2ZGNrajZkcTdmdGg5ZHFhZ2NiN2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZXFpYmpuanZoY2JibGtxanV1cThma2FicW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=YWlhcnEza3Bjc2plcTdoNGRncTRqMTdxZTRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NXBnaGtmZzFkODU2amNhOGFjN3JzazgwYjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=cGtycG5sbWVmdG80bHRiazduNWtxa2QzYW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=OTQzb2F2MDBkcmpnMXBhczdsaGk3ZjRmZGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ajhvbjhsOW5qYnQ5YTdsOXViNTMyNnQ5NXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ajhvbjhsOW5qYnQ5YTdsOXViNTMyNnQ5NXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bzRkZ3JvN2JxNThqZXJsNmIyMTh1Y20zYnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z3I1N2hucDQ5YjVuZGVpcm8zbDhhbXF1M29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=YjBnNTRiMDlyaTZrcTZrYmZzcWZqb3NpNnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=a2drZ2ZkY3IxaWtjYmMxcTMwa3E4MjVxMTRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MDd0MWF1ajl2OGluZjRhZHUxYmp1cnVtZzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZGFvbTlvM2VwNWw2ZnZmbnNvbDNkbXYxZHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dXNzOG5vMzMxajBsMDF0bzhzdHQ4dWYwZzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MmM4dmJvdW5xYjdhcmliY2tmOWZxb2Vvc2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=amd1bzNxbHBybjFsamNvMWhqZTVlZjNna3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z3V1Zjc4M2t2Z2kzN29mbjQ4OWU5N24xYWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bzQ2NWwxa2VodjJmMG5wdjBqZm1ndTl0bzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Nmk1bWs5ams4YjBrcWJtcjVzdWx1c2R0MWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ajQ3Y29jbTZiOHE5Ymxua3Vzb3FxNzU0bzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NmtxNzZuYzIwZ3I0bGMxdnZscGRtZDZiZmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MGRscGZrdnFiMTZ0ZXNlbDRoY3QzcGVkMWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZW84djJjMGRvazNtb3J1OGNqOW90c2lwN2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=OW03MHJvY2UxMzZrYnBnMmpzNHBlcDRqZ2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZG0zb3ZocGFpcWdvajg1NDZlanYwaTlpcThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=aWJtNmplcjQzc2xra3NxOWhjbXJsMm92YmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MzNsZWc4bHFxZ2t0cWVwM29ybzF0dmE4Nm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ODNtMms5Mm1rN25pdTdqaGwxY25ob2tuM2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dXNrbjlhODgwMXJqY2p0NW90aGFyOTlmYmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const ECE2 = [
+  "https://calendar.google.com/calendar?cid=aDZhb3NnZ2g1Zm1xN2JmZWM0bmw5NWJvZGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bzBrMDVvczRybWFiNHYyMWRnMnQ3ZTVnNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MnVoMThrdDcxZjBzZXBmMjhmOGdoM3E4bzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=amM1cTRuNXBpYnRjZ20yNnY5aDY3OGllOGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z205bmk3bjU3dGk0dWljZDlkYWtmazQ5M2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZW5kcTJuMG45bGVndG4xNW9kbzZtNmF1MjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NWs1Z24zcGJvbzh2c2Q5ajFoZ2pyY2xobzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=czVsbGpyY2hudDdrbW84azF2aWIwMWc5MjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NjRmaWd0bmlxOWlqMDhiNWtjdmkyNmQwM29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=am11cWx2MjI5dTdyNm92bDd1OGlvOXRrbHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const ENC2 = [
+  "https://calendar.google.com/calendar?cid=dTU0a3NtMHNwdmJvcjB0MTkxdDVvZDd2OWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=OGxqZmhqNzlsZnF1NGt1bmlwMjNrdXZ0M2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZTZvMmQwZ25sNGcyNTVlYm11a3Nmcm81dWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dWRqczM5cWlpb2NqNm5pdDZncDBhbTc4amdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bWx1bm5jOGt0c2QyM2JuZ2pscHRhN2Y5M3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Y3JtdW9sbHNpc2E1Y3BvZ21wbHZrNTMzcjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=c2o3bWVocWVhZmYwYjZrNWJzcnNpbjlvZ29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=YWhjbDI0NmxmMm50cHJlcjVjcDZ0bjc1YzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const CHE2 = [
+  "https://calendar.google.com/calendar?cid=bjVwcG5zODEyYTNldWczMGo1bnRvZmEzOGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const CIE2 = [
+  "https://calendar.google.com/calendar?cid=cXNyYzhqc2ExcXFiM3AyZnBqYnAxN3FpNzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=OHE4dWU1dmp0MmxycXF1MDBvbDJvaG81aGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=OGttNDJmNGp0dW9hZmZpMGFmbHR1OHF0c2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const EIC2 = [
+  "https://calendar.google.com/calendar?cid=dGluMTNnMnNycXE5dWxxdG5ydDRmZWo1ZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dmhyanFwYmo2Z2dxZWdoZGNna2ZqcnQ4bm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const ELE2 = [
+  "https://calendar.google.com/calendar?cid=OXQzNnVsMzVqajlrZ3U5dnY5MnY1ZHBvMzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=aTFkZTYzYmlvZGxtZjJsdG1iNTRmbGc0dGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZmFiMjZyamdybDljZGxja2ZnNG01b2JwZmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=aDh0M2puMHRsbjNkZWhqbjhnMG8wcWdzbWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const MEE2 = [
+  "https://calendar.google.com/calendar?cid=ZTlyOGRxYnFpdjEzc21qZTg5cjlwdmtsZzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bDB0ZjFib2Jja2JibTJoZmZsOW9lY3FrdjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NzZobXFqMmkzOHBtMGo3ZWY5ZGQ2dGsyZnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NDNraWdsM25oMDhyOXBwaW43Z2U5MDdzbzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=bHEwb2hwNmExdTJtcnR1ajFwdTFyZHVrcWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=ZGd0YnRpdjQ4dWd2a2x1ZHJkNWlubDFvcTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=MnJycXByc3M3MnNmZmpmbGg4ZmdiOW9mMjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=c2dxOXMwMG1kYWthOTF0aTJocXVna3U1Zm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=NHJ0bGtwczRuMzdnODJ0cGtiZ2pjc2FvcmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z2g1bnZyazE1N2FvOGhjdG5pazlib29nY3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=YWJobXNtajhuZ2JsNWVobjk1bDg0ZWM2NW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=dGpncG8xZm1hZzFnMGM5bXNkbG5nZTlidmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const MTX2 = [
+  "https://calendar.google.com/calendar?cid=a2I2MThndm1qajFzazIydWtyMDRsZjA2dGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z3Vmc3ZqaTZlbmNhbGpybGV2dmEydjFvNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
+
+const BTD2 = [
+  "https://calendar.google.com/calendar?cid=bjc5aHRrYTVmcWVjMTBmYWVzZW9pY2pwMGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=Z3UxOW1tZDhvazh0dTE1bTRrMGg5NmhqMWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+];
