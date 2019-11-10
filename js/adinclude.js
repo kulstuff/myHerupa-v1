7df13e1a2579dccbcb7cf1bc7b0412467a189189
aadi <aadiyaara@gmail.com>
2019-11-11 03:12:59 +0530

    Testing ad fixes #24

--- js/adinclude.js
+++ js/adinclude.js
@@ -1,10 +1,10 @@
-var ins   = document.createElement("ins");
-ins.setAttribute('id','one');
-ins.setAttribute('class','adsbygoogle');
-ins.setAttribute('style','display:inline-block;width:275px;height:152px');/*add other styles if required*/
-ins.setAttribute('data-ad-client','ca-pub-4625423558686989');
-ins.setAttribute('data-ad-slot','2712833606')
-document.getElementById('one').appendChild(ins)
+// var ins   = document.createElement("ins");
+// ins.setAttribute('id','one');
+// ins.setAttribute('class','adsbygoogle');
+// ins.setAttribute('style','display:inline-block;width:275px;height:152px');/*add other styles if required*/
+// ins.setAttribute('data-ad-client','ca-pub-4625423558686989');
+// ins.setAttribute('data-ad-slot','2712833606')
+// document.getElementById('one').appendChild(ins)
 var ins2   = document.createElement("ins");
 ins2.setAttribute('id','two');
 ins2.setAttribute('class','adsbygoogle');
@@ -14,11 +14,11 @@ ins2.setAttribute('data-ad-slot','4820122198')
 document.getElementById('two').appendChild(ins2)
 
 
-var inlineScript   = document.createElement("script")
-inlineScript.type  = "text/javascript";
-inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'
+// var inlineScript   = document.createElement("script")
+// inlineScript.type  = "text/javascript";
+// inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'
 var inlineScript2   = document.createElement("script")
 inlineScript2.type  = "text/javascript";
 inlineScript2.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementById('one').appendChild(inlineScript)
+// document.getElementById('one').appendChild(inlineScript)
 document.getElementById('two').appendChild(inlineScript2)
