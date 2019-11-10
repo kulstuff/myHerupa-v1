5dff27dc6707b639bc4cd5e6c69a4074d8f034e3
aadi <aadiyaara@gmail.com>
2019-11-11 02:13:08 +0530

    Testing ad fixes

--- js/adinclude.js
+++ js/adinclude.js
@@ -1,18 +1,12 @@
-var externalScript   = document.createElement("script");
-externalScript.type  = "text/javascript";
-externalScript.setAttribute('async','async');
-externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementsByTagName('body')[0].appendChild(externalScript);
-
 var ins   = document.createElement("ins");
 ins.setAttribute('class','adsbygoogle');
 ins.setAttribute('style','display:block;');/*add other styles if required*/
 ins.setAttribute('data-ad-client','ca-pub-4625423558686989');
 ins.setAttribute('data-ad-slot','7408371487');
 ins.setAttribute('data-ad-format','auto');
-document.getElementsByTagName('body')[0].appendChild(ins);
+document.getElementsById('one').appendChild(ins)
 
 var inlineScript   = document.createElement("script");
 inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
-document.getElementsByTagName('body')[0].appendChild(inlineScript); 
+document.getElementsById('one').appendChild(inlineScript)
