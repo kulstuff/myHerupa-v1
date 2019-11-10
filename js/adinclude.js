40384d4ea37ba4086aef1a47a564fcfc90508ec3
aadi <aadiyaara@gmail.com>
2019-11-11 02:47:07 +0530

    Testing ad fixes #11

--- js/adinclude.js
+++ js/adinclude.js
@@ -5,8 +5,8 @@ ins.setAttribute('data-ad-client','ca-pub-4625423558686989');
 ins.setAttribute('data-ad-slot','7408371487');
 ins.setAttribute('data-ad-format','fluid')
 ins.setAttribute('data-ad-layput','in-article')
-
 document.getElementById('one').appendChild(ins)
+
 
 var inlineScript   = document.createElement("script")
 inlineScript.type  = "text/javascript";
