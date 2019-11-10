087d716a2a08eb88a0e4ccf5ddc2fdee7fecba93
aadi <aadiyaara@gmail.com>
2019-11-11 04:01:43 +0530

    Testing ad fixes #32

--- js/adinclude.js
+++ js/adinclude.js
@@ -1,8 +1,23 @@
+var externalScript1  = document.createElement("script");
+externalScript1.type = "text/javascript";
+externalScript1.setAttribute('async','async');
+externalScript1.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('two').appendChild(externalScript1);
+
+// variables
+var listDocAdIDs = ['one', 'two', 'three', 'four', 'five']
+var listAdsenseFixedAdsIDs = ['2712833606', '4820122198', '8432558989', '930665695', '7405938849']
+
+// Loop
+
+
+
+
 var externalScript  = document.createElement("script");
 externalScript.type = "text/javascript";
 externalScript.setAttribute('async','async');
 externalScript.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-document.getElementById('two').appendChild(externalScript);
+document.getElementById('two').appendChild(externalScript2);
 
 var ins2 = document.createElement("ins");
 ins2.setAttribute('id','two');
@@ -16,3 +31,22 @@ var inlineScript2 = document.createElement("script")
 inlineScript2.type = "text/javascript";
 inlineScript2.text = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('two').appendChild(inlineScript2)
+
+var externalScript3  = document.createElement("script");
+externalScript3.type = "text/javascript";
+externalScript3.setAttribute('async','async');
+externalScript3.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.getElementById('three').appendChild(externalScript3);
+
+var ins3 = document.createElement("ins");
+ins3.setAttribute('id','three');
+ins3.setAttribute('class','adsbygoogle');
+ins3.setAttribute('style','display:inline-block;width:300px;height:150px');/*add other styles if required*/
+ins3.setAttribute('data-ad-client','ca-pub-4625423558686989');
+ins3.setAttribute('data-ad-slot','4820122198')
+document.getElementById('three').appendChild(ins3)
+
+var inlineScript3 = document.createElement("script")
+inlineScript3.type = "text/javascript";
+inlineScript3.text = '(adsbygoogle = window.adsbygoogle || []).push({});'  
+document.getElementById('three').appendChild(inlineScript2)
--- js/resource.js
+++ js/resource.js
@@ -481,7 +481,7 @@ $(document).ready(function() {
             // Append the files one by one
             files.map((file, index) => {
               if(index == 0) countAd = 0
-              if((index % 5) == 3) insertAdTile(1, 'Advertisement')
+              if((index % 5) == 3 && countAd < 4) insertAdTile(1, 'Advertisement')
               document.getElementById("resource-grid").append(file.elem)
               if(index == (files.length - 1)) {
                 insertAdTile(1, 'Advertisement')
@@ -504,7 +504,8 @@ $(document).ready(function() {
     // files.reverse()
     
     files.map((file, index) => {
-      if((index % 5) == 3) insertAdTile(1, 'Advertisement')
+      if(index == 0) countAd = 0
+      if((index % 5) == 3 && countAd < 4) insertAdTile(1, 'Advertisement')
       document.getElementById("resource-grid").append(file.elem);
       if(index == (files.length - 1)) {
         insertAdTile(1, 'Advertisement')
