0b6c9cc89f4a4128fb5074a91f8aef9c4d6541be
aadi <aadiyaara@gmail.com>
2019-11-11 02:48:06 +0530

    Testing ad fixes #12

--- js/resource.js
+++ js/resource.js
@@ -189,7 +189,7 @@ var tileAds = [
 
 // test Function to print article vars
 setTimeout(function () {
-  console.log('Width: ', $('#resource-element').width() , ' Height: ', $('#resource-element').height())
+  console.log($('#resource-element'))
 }, 15000)
 
 // Day Night Toggle
