611b1efa1ca3c1b643eec4bb618e2c0be5fec735
aadi <aadiyaara@gmail.com>
2019-11-11 02:26:38 +0530

    Testing ad fixes #5

--- js/adinclude.js
+++ js/adinclude.js
@@ -3,10 +3,10 @@ ins.setAttribute('class','adsbygoogle');
 ins.setAttribute('style','display:block;');/*add other styles if required*/
 ins.setAttribute('data-ad-client','ca-pub-4625423558686989');
 ins.setAttribute('data-ad-slot','7408371487');
-ins.setAttribute('data-ad-format','auto');
+ins.setAttribute('data-ad-format','auto')
 document.getElementById('one').appendChild(ins)
 
-var inlineScript   = document.createElement("script");
+var inlineScript   = document.createElement("script")
 inlineScript.type  = "text/javascript";
 inlineScript.text  = '(adsbygoogle = window.adsbygoogle || []).push({});'  
 document.getElementById('one').appendChild(inlineScript)
