acc641142035249f7b4ef01a7fd6ddb5e4f73f24
aadi <aadiyaara@gmail.com>
2020-01-07 11:38:21 +0530

    Added new Calendar links for first year

--- js/timeTableLinks/ttLinksFirst.js
+++ js/timeTableLinks/ttLinksFirst.js
@@ -1,107 +1,107 @@
 const A = [
-  "https://calendar.google.com/calendar?cid=dmw1MW9kaXQ0NmRnNjNrYzQxdjh1bTI5a29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=ZGlyOXZoNzI5bmsybHBkZ2RrZGM2ZDM3bWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MzVtNDltbnZsZjJsa29kYzFiazZuYTJzb2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=cWdyZW1zMnQ0aWx2dTlnOGN2ZW5uY2UxZDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MnRlYWkwdThwbHJjN2Nucm10cmFmMmNrNW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=te4fp6o1b2qurs509gkic685i0@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=mee9180rr468300f0jqfid7jf4@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=16kc92dlcrsvva6kbukkmik320@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=rh1o154gksbt403hke6ergrnvk@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=2iks4bbseqg0sg9uo9etpjgekc@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=5clvve7n6a8o2lsa41n1t4rs3k@group.calendar.google.com"
 ];
 
 const B = [
-  "https://calendar.google.com/calendar?cid=ZXUxcm1yaWthcG1idjZvbTR0dHQ0bnE0Y2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MHFxdGduZHM3MjFycGpycXZqNWlpMnZ0aXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MzljbmVtaXRldWUwN2ZzdHVtbDRsdGc2MmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=a2YzZHUzYzZuMWlmaHI2MWtqM3ZrbzVkOGNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=ZzcxdG9ka29hOTQwN2R1b3IyaWhnOXJoMWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=h0ajku7hl7nsfe2tu9ss3fo9so@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=duokh4ktucg5mjq027shr17vfs@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=vje5ismpeqj26po6kj7o0n0b3g@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=5u6gveqsnutp0vgtava5cd8604@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=t2o978brglu2m4kabvsa70j5ck@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=ukbkmof8784nloc09vmkjk8lhs@group.calendar.google.com"
 ];
 
 const C = [
-  "https://calendar.google.com/calendar?cid=Yjk4ZHRzcmw1M2xma2l0MjFnNGhoZmlhajhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MmNibmNmMDNyOG1lMnVucGs4Mmd2NnF0ZzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=YmdkM3RrcDZ1YTBxMTQxODJyM3Uxc3VmYTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NG8xMjlnaDJrdHRkODQ3NzlwMjlkc2Rtc2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=ZjdwbGo3YTVjZTcxNWNzbzlyN2R2M2wyYXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=vqt8l3mqgvi6t8q8vs1qnpfin4@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=lvbmjp9d4si68fkdocko0u161k@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=aolm75c5akevjm7sp8o764kodg@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=85qi8mv2pouldjmk5hh6l0v06c@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=jv3qgp3r828bngoo0egtb60e04@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=fgq6itjuk0l2vnb4qnsf0kh6uo@group.calendar.google.com"
 ];
 
 const D = [
-  "https://calendar.google.com/calendar?cid=YWpxNDl2YXVham0ydG51OTNyM2luZ3FwZDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=aGZoNGk0dG1kY2MzdWxsaGNuMTd0aWlkazhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=dGFnMnR0dnI3ZGgzdGJ0MXNpdnJmazA2MzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NmplZzQ3ZnR0MXY5bHYzNDk1NWtkYjRuZ3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=dDBldWNiamk2YzdzN2UxdW1waWY2dHBxYnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=3elu0rshhl0slncl7hnkc7c0f8@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=42jd2gs4pglggfli6oujdnr3sc@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=5pnr5lopvvviiksl3o7i39953s@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=775t6obqa8irn0btfj5vm67tag@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=8i604jion82jburnth2abugjhc@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=2teai0u8plrc7cnrmtraf2ck5o@group.calendar.google.com"
 ];
 
 const E = [
-  "https://calendar.google.com/calendar?cid=b3QxcXBuczkzaTM0ZnY5MnV0ZmMyMDNwaWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=c3NwNGVscGFjcm9taG8waGdmZW12N3E5cjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=M2VqNTQ0cjRpcDhscmdicThoYWljaW05b2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=N2tlZ2Q4dWNzcnBwdmFvZm12bzVwNXU0OWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NTcyM2tlZXN1czc2djhkdHV2NWgxZ2JmZmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=lbu6o1jp99iicm3lis383etq8o@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=a1n17sjhpgajnbukvogbe1pqdc@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=fjlp8cad22bd7ondlm34tbcfrk@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=8ieqgv2v66jv1kvnp6dheki0l0@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=j9e8f0vn74rtlu03tsbu8fm3bk@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=nrvlnt1anmq5g903k2mt0m1r2o@group.calendar.google.com"
 ];
 
 const F = [
-  "https://calendar.google.com/calendar?cid=dWs4aDViYzhncGczNG12NXI5bWh2YTA4NDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=YTBkdmM3NGJpMWZqaTRmbjgyODF2bmlxZDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=YTk5NTBxMms1YXEzNHAxZ2k1YmxkZWt2bThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bmVhZTd1NWN0ZzFubW9hYjNhc3EzdWprbThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=amk5bmNkNmM5cHZxZWJlZHJmY2VmZmFlcW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=pism2d2qvlhs41dm6co6ag2t8o@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=mk914eddr0urf9mfb6v3n6jurg@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=7i9bc7vder0j1tnsirjmt70pho@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=n9ad6g46f1lteq3ml0v4kr7vqo@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=5i4hndj5uc1untb58bvvoaaegs@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=vidp9i1n59l3a5ion94phsl72c@group.calendar.google.com"
 ];
 
 const G = [
-  "https://calendar.google.com/calendar?cid=MnZuZ2lmb3V2bnM1MnJpdnZhMmxwZDBtcDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=dGxvcWxvdmtnbTMxbXNmZ3I3YjVmbDdra2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NTY3OHJ0NTFwM21hbTVocDB0aTk2M21ha2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=amhlZ2dncTFpNXQ3bjJhZGIwb3ViMGo1a2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bDZqbGl0dTQxaG1jM201YTlmN3Noc3NwaWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=lq1rov646gl6ca1r2hlcgn1bdk@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=4g6178bm438nihb1ilsq8stjv8@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=a7mdajthh1fgfmca6dr747kkq8@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=onktmumu6475nkv1c8l5if9okk@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=56j4h2k7uv0neqsgl85bdn73ts@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=5rgc6eibo3rd9h2qm1shaid3pk@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=4r9jb9uch0bkj4e1us1811570c@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=3srglngn6q1m29icb9mja1r4p0@group.calendar.google.com"
 ];
 
 const H = [
-  "https://calendar.google.com/calendar?cid=M2p2aHBndGdpczIwOXRvNmxoa2Z1MXNua29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=OTdiaG41MjQ4Z3ZlYmoxa2VyZnRsMnY2Z29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=aXVrYm1oaHR0dDhnbzllZjhmZXM0dTJpODBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bnJxanJuaTI1M3FqZ3N2cXM1c3VsMDQxczRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bGxyZW5xbDY3dGllaDc2ZmxlM3V2Z3BiZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=3cv9oprcn0chje8okf11hncods@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=mscl38fdd9n6botf08i04ettjg@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=5p5r42m1veihu9m4uvckvjbl00@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=dh6n561k6an5fnrjorobvnjj98@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=n35v0veu5qb9m5o9egv6duo26g@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=17k4jcso81bm9b729n2vl907qo@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=5hoctanaqopg8s57rhvkhonp8c@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=cj804jjpv62iliqe89hp64220s@group.calendar.google.com"
 ];
 
 const I = [
-  "https://calendar.google.com/calendar?cid=b3Zzc29mamMyNXM1dTYzbzk5NGd1amRkZjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bW9iMzU3M2g1OTZ0cDBvcjl1a2dvNnI1bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=cWN1bTZmcm9iMWVjMGxqYmc0dDdzZ2ljaThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MWhjZmdyNnZ0Yzc2bjdrYjkzY3Axbm9odG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=YWY4NHYyaWY0NGlkNWxoZDZzOWEwMWVsMDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=72jttvehcili3hpo2ijhro2kbk@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=0sb1u0c4802qeba62ii2fmehrc@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=1a0n6b8al2c6tsl44huimetbtc@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=kp0muuvvaar7mdjcmag2nf9vm0@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=laqtqaq7rd5mr1022ee5fju2ls@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=ch5heas1hm7pptk1v4j66hbkak@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=ag3gi2fu4ajlkic6tel9uvi3ug@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=c1oa1h7jiud1fi8t7958j7cqg4@group.calendar.google.com"
 ];
 
 const J = [
-  "https://calendar.google.com/calendar?cid=ZW81OG1zcGcycTl1c3Z2cDBjNmdib3BtaWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=YjVoN2JvajhqYmYydWhpZmdnM2NpbWpnNXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=MDEwdTYxNmZnbG1sNGkxdmdnbDN0ZWkxbXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bjU5dm5mdjdnOTludmJsaHFhM2FiMG84MnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=cnJwaDlrcjE4bnZxazZkOW9xYzE0ajdkamNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=lv6057hrpnijfl6e5jmuk65iis@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=epgu52dj9vvihv9d16qoj6qou4@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=6p4j2iu8p6skac0ni6jdn95vhs@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=80qk17k24s0saj8dfdpi6c5q70@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=j1lr4nkp5os85d5algdrqnuj8o@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=7oig72mo37nosi8ntras976kf8@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=2kapsjbftqbpqpbvtts186ledk@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=1caq3uiegq7bk11bp5ipbv7s4s@group.calendar.google.com"
 ];
 
 const K = [
-  "https://calendar.google.com/calendar?cid=ZWIwNG05dWlwcWxzOHIzMHEya3VxOGQ3ZThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NWVkamRlcnM0YmxjNmFuMjlxZmllZWo0azBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=NWY3MTVkdTA2NHZuajhqZmsxdTJpa3E2dm9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=bnE0ZjlybXBzZzk0OWNjcWk4ZzVhb284bnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
-  "https://calendar.google.com/calendar?cid=azNoZ3FkZmZsYjZhODQ3YmE5aDRmbzg0b2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+  "https://calendar.google.com/calendar?cid=h4crpoou2unoklt3v374oppom8@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=fgltp6pggq35sn31i8bfm63r8o@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=83n849fbbr0nuk2na1r8532hb4@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=o1f1igrojt9ii8vv3g19p2u4a8@group.calendar.google.com",
+  "https://calendar.google.com/calendar?cid=4rf65pke5o0ek53pvboop52tcs@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=6ll60tr6rivgdf4oj1kg8uo5uc@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=kvo571kkabpariin1b56g7erb4@group.calendar.google.com",
   "https://calendar.google.com/calendar?cid=c427oh6m2jlt11m4d5bv2eapb8@group.calendar.google.com"
